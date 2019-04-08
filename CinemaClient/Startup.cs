5f5cd5e9119a3e61c454715b5e97f07ca5d67d4d
Lech, Piotr <piotr.lech@accenture.com>
2019-04-08 10:46:51 +0200

    [ADDED] Static files handling & default routing

--- CinemaClient/Startup.cs
+++ CinemaClient/Startup.cs
@@ -10,6 +10,7 @@ namespace CinemaClient
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -19,6 +20,15 @@ namespace CinemaClient
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
         }
     }
