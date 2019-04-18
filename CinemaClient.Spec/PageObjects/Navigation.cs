0479d500a62313f9673aabe2c3bdd03bbd4f6414
Lech, Piotr <piotr.lech@accenture.com>
2019-04-18 20:29:53 +0200

    [ADDED] Page object pattern implementation for navigation module

--- CinemaClient.Spec/PageObjects/Navigation.cs
+++ CinemaClient.Spec/PageObjects/Navigation.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using OpenQA.Selenium;
 
 namespace CinemaClient.Spec.PageObjects
 {
-    class Navigation
+    public class Navigation
     {
+        private readonly IWebDriver _visitorBrowser;
+
+        private const string NavigationBarSelector = "//nav[@class='navigation']/div[@class='navigation-panel']";
+        private const string LogoSelector = NavigationBarSelector + "/span[@class='navigation-logo']";
+        private const string HomePageBtnSelector = NavigationBarSelector + "/a[1]";
+        private const string ProgramPageBtnSelector = NavigationBarSelector + "/a[2]";
+        private const string EventsPageBtnSelector = NavigationBarSelector + "/a[3]";
+        private const string NewsPageBtnSelector = NavigationBarSelector + "/a[4]";
+        private const string ContactPageBtnSelector = NavigationBarSelector + "/a[5]";
+
+        public IWebElement NavigationBarElement { get; set; }
+        public IWebElement LogoElement { get; set; }
+        public IWebElement HomePageBtnElement { get; set; }
+        public IWebElement ProgramPageBtnElement { get; set; }
+        public IWebElement EventsPageBtnElement { get; set; }
+        public IWebElement NewsPageBtnElement { get; set; }
+        public IWebElement ContactPageBtnElement { get; set; }
+
+        public Navigation(IWebDriver visitorBrowser)
+        {
+            _visitorBrowser = visitorBrowser;
+        }
+
+        private void FindNavigationElements()
+        {
+            NavigationBarElement = _visitorBrowser.FindElement(By.XPath(NavigationBarSelector));
+            LogoElement = _visitorBrowser.FindElement(By.XPath(LogoSelector));
+            HomePageBtnElement = _visitorBrowser.FindElement(By.XPath(HomePageBtnSelector));
+            ProgramPageBtnElement = _visitorBrowser.FindElement(By.XPath(ProgramPageBtnSelector));
+            EventsPageBtnElement = _visitorBrowser.FindElement(By.XPath(EventsPageBtnSelector));
+            NewsPageBtnElement = _visitorBrowser.FindElement(By.XPath(NewsPageBtnSelector));
+            ContactPageBtnElement = _visitorBrowser.FindElement(By.XPath(ContactPageBtnSelector));
+        }
+
+        public void CheckIfNavgationElementsExist()
+        {
+            FindNavigationElements();
+            NavigationBarElement.Should().NotBeNull();
+            LogoElement.Should().NotBeNull();
+            HomePageBtnElement.Should().NotBeNull();
+            ProgramPageBtnElement.Should().NotBeNull();
+            EventsPageBtnElement.Should().NotBeNull();
+            NewsPageBtnElement.Should().NotBeNull();
+            ContactPageBtnElement.Should().NotBeNull();
+        }
     }
 }
